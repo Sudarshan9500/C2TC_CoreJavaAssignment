e937afd7fb6c9af85fa55500a69b125ca48d4af6
selva <selva@DESKTOP-1R3F6R3>
2025-08-20 15:53:03 +0530

    commit

---
+++ Assignment1/src/com/sudarshan/assignment2/AssignmentMain2.java
@@ -0,0 +1,15 @@
+package com.sudarshan.assignment2;
+
+import java.util.*;
+
+
+public class AssignmentMain2 {
+      public static void main(String[] args) {
+    	  
+    	  Student s=new Student();
+    	  Commission emp=new Commission();
+    	  
+    	  emp.acceptDetails();
+    	  emp.toCalculate();
+      }
+}
---
+++ Assignment1/src/com/sudarshan/assignment2/Student.java
@@ -0,0 +1,8 @@
+package com.sudarshan.assignment2;
+
+class Student{
+	
+	public void Student() {
+		System.out.println("Student Object Created...");
+	}
+}
