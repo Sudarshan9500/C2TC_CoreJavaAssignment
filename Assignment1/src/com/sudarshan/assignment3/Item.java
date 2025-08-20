6b677b3271fd12d862d2c0fced17c212a36434fc
selva <selva@DESKTOP-1R3F6R3>
2025-08-20 16:03:37 +0530

    commit

---
+++ Assignment1/src/com/sudarshan/assignment3/Item.java
@@ -0,0 +1,17 @@
+package com.sudarshan.assignment3;
+
+public class Item {
+	 protected int id;
+	    protected String title;
+
+	    // Constructor
+	    public Item(int id, String title) {
+	        this.id = id;
+	        this.title = title;
+	    }
+
+	    // Method to display info (to be overridden in subclasses)
+	    public void displayInfo() {
+	        System.out.println("Item [ID: " + id + ", Title: " + title + "]");
+	    }
+}
